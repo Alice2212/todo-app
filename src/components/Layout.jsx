c486d0f4de3ce0834c29ccff565ebe027d37091d
Oluwatosin <hezealice@gmail.com>
2022-12-06 11:57:56 +0100

    completed layout of the app

---
+++ src/components/Layout.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Layout.css";
+import bgimg1 from "../assets/images/bg-desktop-dark.jpg"
+import sun from "../assets/images/icon-sun.svg"
+
+function Layout() {
+  return (
+    <div className='container'>
+        <div className='header-wrapper'>
+            <img src={bgimg1} alt="backgroundImage"/>
+            <div className='heading'>
+                <h1>TODO</h1>
+                <img src={sun} alt="theme-icon"/>
+            </div>
+        </div>
+        <div className='down-wrapper'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Layout
